70864d888c26cfc82b548616adb7137ea867df00
Linna <1500012782@pku.edu.cn>
2017-11-01 19:00:02 +0800

    Finished building the model training the model.

--- LearningKeras/LearningKeras.py
+++ LearningKeras/LearningKeras.py
@@ -18,3 +18,18 @@ X_train, Y_train = X[: 160], Y[: 160]
 # Take the last 40 datas as testing data
 X_test, Y_test = X[160:], Y[160:]
 
+# Build up the model
+print('Building-------')
+model = Sequential()
+model.add(Dense(output_dim=1, input_dim=1))
+
+# Choose the loss function and optimizing method
+model.compile(loss='mse', optimizer='sgd')
+
+# Training the model
+print('Training-------')
+for step in range(301):
+    cost = model.train_on_batch(X_train, Y_train)
+    # After 100 steps, print the train cost out
+    if step % 100 == 0:
+        print('train cost', cost)
